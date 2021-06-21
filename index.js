d90142e51a2e82914c11fd2414a15a19d56ddf61
ialmeida <ialmeida.pb@gmail.com>
2021-06-21 16:03:38 +0100

    updated css

--- index.js
+++
@@ -1,9 +0,0 @@
-let firstCard = document.getElementById('firstCard');
-let secondCard = document.getElementById('secondCard');
-let thirdCard = document.getElementById('thirdCard');
-
-
-window.addEventListener('scroll', function(){
-    let value = window.scrollY;
-    firstCard.style.right = value * 1.05 + 'px';
-});
