d42fb4ac366e2985cde187f76f64c576448a1919
ialmeida <ialmeida.pb@gmail.com>
2021-06-24 19:29:18 +0100

    updated

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-
+document.querySelector(".slider-recipes").addEventListener('wheel', function scroll(event){
+    if(event.deltaY>0){
+        event.target.scrollBy(300, 0)
+    }else{
+        event.target.scrollBy(-300, 0)
+    }
+})
